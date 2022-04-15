afd648770be11c8ff398c106e10ebbb39b3c574d
Ernest Bruno <erndobruno@gmail.com>
2022-04-15 14:59:09 -0700

    Import Base64 library to encode data. Mint an NFT to test and return JSON data.

--- contracts/EpicGame.sol
+++ contracts/EpicGame.sol
@@ -1,28 +1,46 @@
 // SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.0;
 
+// inherit from this. Thanks Open Zeppelin!
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// helper functions
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "hardhat/console.sol";
+import "./libraries/Base64.sol";
 
-contract EpicGame {
-
+// Contract inherit from ERC721, a standard NFT contract
+contract EpicGame is ERC721 {
     // Create a schema/struct for character attributes.
     struct CharacterAttributes {
         uint characterIndex;
         string name;
         string img;
-        string class;
+        string charClass;
         uint hp;
         uint maxHp;
         uint attackDmg;
         uint exp;
         uint maxExp;
         uint lvl;
+        uint dodgeChance;
     }
+
+    // tokenId is NFTs unique identifier, a number that goes 0, 1, 2, 3...
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     // Create an array of CharacterAttributes;
     // Hold default data for characters.
     CharacterAttributes[] defaultCharacters;
+
+    // Create mapping from nft's tokenId => that NFTs attributes.
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // A mapping from an address => NFTs tokenId. Gives ez way to store owner of NFT and reference later.
+    mapping(address => uint256) public nftHolders;
 
     // Data passed in to the contract when it's first created initializing the characters.
     // Actually pass values from run.js
@@ -30,26 +48,126 @@ contract EpicGame {
         string[] memory characterNames,
         string[] memory characterImageURIs,
         string[] memory characterClass,
-        uint[] memory characterHp,
-        uint[] memory characterDmg
-    ){
+        uint256[] memory characterHp,
+        uint256[] memory characterDmg,
+        uint256[] memory characterDodge
+    ) ERC721("RANDOM HUNTER", "HNTR") {
         // Loop through characterNames, and save values in contract.
-        for (uint i = 0; i < characterNames.length; i+=1){
-            defaultCharacters.push(CharacterAttributes({
-                characterIndex: i,
-                name: characterNames[i],
-                img: characterImageURIs[i],
-                class: characterClass[i],
-                hp: characterHp[i],
-                maxHp: characterHp[i],
-                attackDmg: characterDmg[i],
-                exp: 0,
-                maxExp: 100,
-                lvl: 1
-            }));
+        for (uint256 i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    img: characterImageURIs[i],
+                    charClass: characterClass[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDmg: characterDmg[i],
+                    dodgeChance: characterDodge[i],
+                    exp: 0,
+                    maxExp: 100,
+                    lvl: 1
+                })
+            );
 
             CharacterAttributes memory char = defaultCharacters[i];
-            console.log("Done initializing %s w/ HP %s, img %s", char.name, char.hp, char.img);
+            console.log(
+                "Done initializing %s w/ HP %s, img %s",
+                char.name,
+                char.hp,
+                char.img
+            );
         }
+
+        _tokenIds.increment();
+    }
+
+    function mintCharacterNFT(uint256 _characterIndex) external {
+        // get current token id. should be 1 because we increment in constructor.
+        uint256 newItemId = _tokenIds.current();
+
+        // magical function that ssigns the tokenId to the caller's wallet address.
+        _safeMint(msg.sender, newItemId);
+
+        // we map tokenId => characterAttributes.
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            img: defaultCharacters[_characterIndex].img,
+            charClass: defaultCharacters[_characterIndex].charClass,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDmg: defaultCharacters[_characterIndex].attackDmg,
+            exp: defaultCharacters[_characterIndex].exp,
+            maxExp: defaultCharacters[_characterIndex].maxExp,
+            lvl: defaultCharacters[_characterIndex].lvl,
+            dodgeChance: defaultCharacters[_characterIndex].dodgeChance
+        });
+
+        console.log(
+            "Minted NFT w/ tokenId %s and characterIndex %s",
+            newItemId,
+            _characterIndex
+        );
+
+        // keep track who wns what NFT.
+        nftHolders[msg.sender] = newItemId;
+
+        _tokenIds.increment();
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[
+            _tokenId
+        ];
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(
+            charAttributes.attackDmg
+        );
+        string memory strExp = Strings.toString(charAttributes.exp);
+        string memory strMaxExp = Strings.toString(charAttributes.maxExp);
+        string memory strLvl = Strings.toString(charAttributes.lvl);
+        string memory strDodgeChance = Strings.toString(
+            charAttributes.dodgeChance
+        );
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+                '{"name":"',
+                charAttributes.name,
+                " -- NFT#: ",
+                Strings.toString(_tokenId),
+                '", "description": "This is an NFT that lets people play in Random Hunters!", "image": "',
+                charAttributes.img,
+                '", "class": "',
+                charAttributes.charClass,
+                '", "attributes": [ { "trait_type": "Health Points", "value": ',
+                strHp,
+                ', "max_value": ',
+                strMaxHp,
+                ' }, { "trait_type": "Experience Points", "value": ', 
+                strExp,
+                ', "max_value": ',
+                strMaxExp,
+                ' }, { "trait_type": "Attack Damage", "value": ',
+                strAttackDamage,
+                ' }, { "trait_type": "Dodge Chance", "value": ',
+                strDodgeChance,
+                ' }, { "trait_type": "Level", "value": ',
+                strLvl,
+                " } ]}"
+            )
+        );
+
+        string memory output = string(abi.encodePacked("data:application/json;base64,", json));
+
+        return output;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -5,10 +5,24 @@ const main = async () => {
         ['https://imgur.com/JSN12tz', 'https://imgur.com/x0uajPV', 'https://imgur.com/v83cxKd'],
         ['Warrior', 'Archer', 'Berserker'],
         [250, 200, 300],
-        [75, 100, 50]
+        [75, 100, 50],
+        [4, 5, 2]
     );
     await gameContract.deployed();
     console.log("Deployed to:", gameContract.address);
+
+    let txn;
+    // mint character at index 0 from array
+    txn = await gameContract.mintCharacterNFT(0);
+    await txn.wait();
+
+    // get value of nft's uri.
+    // tokenURI is a function on every NFT that returns the actual data attached to the NFT.
+    // saying contract, get me data inside the NFT with tokenId 1.
+    // which is the first minted.
+    let returnedTokenUri = await gameContract.tokenURI(1);
+    console.log("Token URI:", returnedTokenUri);
+
 }
 
 const runMain = async () => {
